6bd4ff2b3cb827874b731a55dc9dc2ac7ce20267
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-02-13 16:22:54 +0600

    feature: setup connection to mongo db

--- server/index.js
+++ server/index.js
@@ -1,14 +1,19 @@
 require('dotenv').config()
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 9090
 const app = express()
 
-app.use(cors)
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL)
     app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`))
   } catch (e) {
     console.error(e)
