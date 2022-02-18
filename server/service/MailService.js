3d2f26b032d385f2ff8ebfb3b03547d4da7efdf8
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-02-19 00:11:25 +0600

    feature: add mail service

--- server/service/MailService.js
+++ server/service/MailService.js
@@ -1,5 +1,31 @@
+const nodemailer = require('nodemailer')
+
 class MailService {
-  async sendActivationMail(email, link) {}
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
+      }
+    })
+  }
+
+  async sendActivationMail(email, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: `Account activation on ` + process.env.API_URL,
+      test: '',
+      html:
+        `<div>
+            <h1>For activation go to the link below:</h1>
+            <a href="${link}">${link}</a>
+          </div>`
+    })
+  }
 }
 
 module.exports = new MailService()
