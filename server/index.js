cfcca95eebff46642cdc373c4d604789c5ba9d75
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-02-20 13:12:57 +0600

    feature: add error middleware

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const router = require('./router/index')
+const errorMiddleware = require('./middlewares/ErrorMiddleware')
 
 const PORT = process.env.PORT || 9000
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
 app.use('/api', router)
+app.use(errorMiddleware)
 
 const start = async () => {
   try {
