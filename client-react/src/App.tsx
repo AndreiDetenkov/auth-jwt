a5af5eff31b703d4fd8582dfa8bfad3f54cbcce8
AndreiDetenkov <andrei.detenkov@somo.kg>
2022-03-13 13:05:56 +0600

    feature: fetch users, refresh token updates

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,10 +1,13 @@
-import React, { FC, useContext, useEffect } from 'react'
+import React, { FC, useContext, useEffect, useState } from 'react'
 import LoginForm from './components/LoginForm'
 import { Context } from './index'
 import { observer } from 'mobx-react-lite'
+import { UserInterface } from './models/response/AuthResponse'
+import UserService from './services/UserService'
 
 const App: FC = () => {
   const { store } = useContext(Context)
+  const [users, setUsers] = useState<UserInterface[]>([])
 
   useEffect(() => {
     const token = localStorage.getItem('token')
@@ -12,6 +15,15 @@ const App: FC = () => {
       store.checkAuth()
     }
   }, [])
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers()
+      setUsers(response.data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   if (store.isLoading) {
     return <div>Loading...</div>
@@ -25,6 +37,13 @@ const App: FC = () => {
     <div className="App">
       <h3>{store.isAuth ? `User authorized: ${store.user.email}` : 'Login'}</h3>
       <button onClick={() => store.logout()}>Logout</button>
+
+      <div>
+        <button onClick={getUsers}>get users</button>
+      </div>
+      {users.map((user) => (
+        <p key={user.id}>{user.email}</p>
+      ))}
     </div>
   )
 }
